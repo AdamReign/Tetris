0f0c625b73c419ac7fe27a173ed985981fb412b3
Andrii Paziuk <87915602+AndriiPaziukUA@users.noreply.github.com>
2024-02-19 20:27:01 +0300

    Create Beans.java

---
+++ src/main/java/org/example/config/Beans.java
@@ -0,0 +1,31 @@
+package org.example.config;
+
+public class Beans {
+//    private static GameController gameController;
+//    private static TetrominoService tetrominoService;
+//    private static TetrominoRepository tetrominoRepository;
+//
+//    public static GameController getGameController() {
+//        if (Objects.isNull(gameController)) {
+//            gameController = new GameController();
+//        }
+//
+//        return gameController;
+//    }
+//
+//    public static TetrominoService getTetrominoService() {
+//        if (Objects.isNull(tetrominoService)) {
+//            tetrominoService = new TetrominoService();
+//        }
+//
+//        return tetrominoService;
+//    }
+//
+//    public static TetrominoRepository getTetrominoRepository() {
+//        if (Objects.isNull(tetrominoRepository)) {
+//            tetrominoRepository = new TetrominoRepository();
+//        }
+//
+//        return tetrominoRepository;
+//    }
+}
